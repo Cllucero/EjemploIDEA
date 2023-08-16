cbce977c219f87c76113720b47b550e931f34322
CLL <claudiolucianolucero@gmail.com>
2023-08-16 18:38:53 +0200

    Clase NIF

---
+++ NIF.java
@@ -0,0 +1,47 @@
+//Crea una clase NIF. Los atributos serán el número de DNI y la letra.
+// La clase contendrá un método privado que calcule la letra del NIF a partir del número de DNI
+
+public class NIF {
+
+    private int numero;
+    private char letra;
+
+
+    public  NIF (int numerox){
+        numero=numerox;
+        letra = letraNIF(numerox);
+    }
+
+
+
+    private char letraNIF(int numeroy){
+       int aux= numeroy % 23;
+        char[] tablaLetra;
+        tablaLetra = new char[]{'Z', 'A', 'Q', 'W', 'S', 'X', 'E','D','C','R','F','V','T','G','B','N','H','Z', 'A', 'Q', 'W', 'S', 'X', 'E','D','C','R','F','V','T','G','B','N','H'};
+        return tablaLetra[aux];
+    }
+    
+
+
+    public int getNumero() {
+        return numero;
+    }
+public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public char getLetra() {
+        return letra;
+    }
+
+    public void setLetra(char letra) {
+        this.letra = letra;
+    }
+
+    public static void main(String[] args) {
+        NIF nif = new NIF(30988055);
+        System.out.println("NIF: " + nif.getLetra()+nif.getNumero());
+    }
+
+
+}
